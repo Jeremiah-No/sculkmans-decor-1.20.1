8a84ed87b553cc1a82bc14aa52d89851ca4d6668
Jeremiah-No <caspian.hedbeck@gmail.com>
2025-06-04 20:31:09 +0200

    Slabs, stairs, and walls for the sculk bone block

--- src/main/java/net/jeremiah/sculkdecor/registry/ModBlocks.java
+++ src/main/java/net/jeremiah/sculkdecor/registry/ModBlocks.java
@@ -1,21 +1,29 @@
 package net.jeremiah.sculkdecor.registry;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.jeremiah.sculkdecor.SculkmansDecor;
 import net.jeremiah.sculkdecor.block.SonicBoomGenerator;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public class ModBlocks {
     public static final Block SCULK_BONE_BLOCK = registerBlock("sculk_bone_block",
             new Block(Block.Settings.copy(Blocks.BONE_BLOCK)));
+
     public static final Block SCULK_BONE_BLOCK_BRICKS = registerBlock("sculk_bone_block_bricks",
             new Block(Block.Settings.copy(Blocks.BONE_BLOCK)));
     public static final Block CHISELED_SCULK_BONE_BRICKS = registerBlock("chiseled_sculk_bone_bricks",
             new Block(Block.Settings.copy(Blocks.BONE_BLOCK)));
     public static final Block SONIC_BOOM_GENERATOR = registerBlock("sonic_boom_generator",
             new SonicBoomGenerator());
+
+    public static final Block SCULK_BONE_BLOCK_STAIRS = registerBlock("sculk_bone_block_stairs",
+            new StairsBlock(ModBlocks.SCULK_BONE_BLOCK.getDefaultState(), FabricBlockSettings.copyOf(Blocks.BONE_BLOCK)));
+    public static final Block SCULK_BONE_BLOCK_SLAB = registerBlock("sculk_bone_block_slab",
+            new SlabBlock(FabricBlockSettings.copyOf(Blocks.BONE_BLOCK)));
+    public static final Block SCULK_BONE_BLOCK_WALL = registerBlock("sculk_bone_block_wall",
+            new WallBlock(FabricBlockSettings.copyOf(Blocks.BONE_BLOCK)));
 
     private static Block registerBlock(String name, Block block) {
         return Registry.register(Registries.BLOCK, SculkmansDecor.id(name), block);
