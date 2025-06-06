1212ab6c5fdbcfc40a17ac1044f8328e167c2746
Zetaclix <zetaclix5@gmail.com>
2025-06-06 22:22:17 +0200

    fix spherecasting using a line instead of segment

--- src/main/java/net/jeremiah/sculkdecor/utils/RaycastUtils.java
+++ src/main/java/net/jeremiah/sculkdecor/utils/RaycastUtils.java
@@ -6,6 +6,9 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 public class RaycastUtils {
     public static <T extends Entity> RaycastResult<T> spherecast(List<T> entities, Vec3d origin, Vec3d end, double maxDist) {
         T closest = null;
@@ -21,8 +24,13 @@ public class RaycastUtils {
         return new RaycastResult<>(closest != null, closest);
     }
 
-    private static double linePointDist(Vec3d a, Vec3d b, Vec3d p) {
-        return b.subtract(a).crossProduct(a.subtract(p)).length() / b.subtract(a).length();
+    private static double linePointDist(Vec3d a, Vec3d b, Vec3d c) {
+        final var ab = b.subtract(a);
+        final var ac = c.subtract(a);
+
+        final var t = max(0, min(1, ac.dotProduct(ab) / ab.lengthSquared()));
+        final var projection = a.add(ab.multiply(t));
+        return c.distanceTo(projection);
     }
 
     public record RaycastResult<T extends Entity>(boolean hit, @Nullable("hit=false") T entity) {
