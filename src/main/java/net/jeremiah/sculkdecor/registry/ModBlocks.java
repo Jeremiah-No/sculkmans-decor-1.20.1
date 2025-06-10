5c21781b82f089f7a5ad1a11a25b471002ee9d89
Zetaclix <zetaclix5@gmail.com>
2025-06-10 22:53:03 +0200

    optimized imports

--- src/main/java/net/jeremiah/sculkdecor/registry/ModBlocks.java
+++ src/main/java/net/jeremiah/sculkdecor/registry/ModBlocks.java
@@ -7,7 +7,6 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldView;
